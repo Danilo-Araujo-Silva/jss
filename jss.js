69092a211eba01a836e38a12995b8eda84fe5dd7
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-01-03 18:53:42 -0200

    Just jss.

--- jss.js
+++ jss.js
@@ -27,32 +27,3 @@ var jss = new function jss() {
 		return JSON.parse(output);
 	}
 }
-
-var json1 = {
-	'@media (max-width: 800px)': {
-		'#all #comments': {
-			'margin': '0px',
-			'width': 'auto',
-			'font-size' : '1.1rem'
-		},
-		'#all #buttons': {
-			'padding': '5px 10px',
-			'font-size' : '1.5rem'
-		}
-	},
-	'#all #content': {
-		'margin': '10% 5% 5% 10%',
-		'width': 'auto'
-	},
-	'#menu': {
-		'border-bottom': '1px solid #ffa500',
-		'margin-bottom': '1.5rem',
-		'font-size' : '1.1rem'
-	}
-};
-
-var css1 = jss.toCss(json1);
-var json2 = jss.toJs(css1);
-
-console.log(JSON.stringify(json2));
-console.log(JSON.stringify(json1) === JSON.stringify(json2));
